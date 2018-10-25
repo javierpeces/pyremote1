e3bd1cc1a9ce5071b2c399961956f2ee84f2bf22
jpeces <jpeces@192.168.1.39>
2018-10-25 23:15:02 +0200

    # WARNING: head commit changed in the meantime
    La rama master, creada por egit

---
+++ pyfile1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+ds = { "clave1": "valor1", "clave2": "valor2", "clave3": "valor3" }
+
+for k, v in enumerate( ds ):
+    print( "k =", k, "v =", v )
